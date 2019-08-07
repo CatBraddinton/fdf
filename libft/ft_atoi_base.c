b65deec148863b96d00f70dc54b663945001a817
Kateryna <katrinmclawren@gmail.com>
2019-08-07 22:48:20 +0300

    recoded ft_atoi_base, now it can take short values

--- libft/ft_atoi_base.c
+++ libft/ft_atoi_base.c
@@ -18,10 +18,10 @@ static int	count_int(const char *s, int base)
 	unsigned long	res;
 	int				i;
 
-	i = ft_strlen(s) - 1;
+	i = strlen(s) - 1;
 	power = 1;
 	res = 0;
-	while (s[i] != 'x' && s[i] != 'X')
+	while ((base == 16 && s[i] != 'x' && s[i] != 'X') || i >= 0)
 	{
 		if (s[i] >= '0' && s[i] <= '9')
 			res = res + ((s[i] - 48) * power);
@@ -35,6 +35,29 @@ static int	count_int(const char *s, int base)
 	return (res);
 }
 
+static int	check_for_short(const char *s, int base)
+{
+	char	mnew[9];
+	int 	len;
+
+	len = strlen(s);
+	if (len == 5 && s[0] == '0' && ((s[1] == 'x') || (s[1] == 'X')))
+	{
+		mnew[0] = '0';
+		mnew[1] = 'x';
+		mnew[2] = s[2];
+		mnew[3] = mnew[2];
+		mnew[4] = s[3];
+		mnew[5] = mnew[4];
+		mnew[6] = s[4];
+		mnew[7] = mnew[6];
+		mnew[8] = '\0';
+	}
+	else
+		return (count_int(s, base));
+	return (count_int(mnew, base));
+}
+
 int			ft_atoi_base(const char *s, int base)
 {
 	int res;
@@ -43,5 +66,7 @@ int			ft_atoi_base(const char *s, int base)
 	while (*s && ((*s == '\t') || (*s == '\n') || (*s == '\f') \
 		|| (*s == '\r') || (*s == '\v') || (*s == ' ')))
 		s++;
+	if (base == 16)
+		return (check_for_short(s, base));
 	return (count_int(s, base));
 }
